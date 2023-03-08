c80269890d8993328589eae274be654de1267dc4
5pamm3r <cmassarino.dev@gmail.com>
2023-03-07 23:38:14 -0300

    First layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,23 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { Box, ChakraProvider, Container, Divider, Heading, Image, Text, VStack } from '@chakra-ui/react'
+import { AppProps } from 'next/app'
+import theme from '../../theme'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const App: React.FC<AppProps> = ({ Component, pageProps }) => {
+  return (
+    <ChakraProvider theme={theme}>
+      <Box padding={4}>
+        <Container backgroundColor='white' boxShadow='md' marginY={4} maxWidth='container.xl' padding={4}>
+          <VStack>
+            <Image borderRadius={9999} src='//placehold.it/128x128' />
+            <Heading>Ecommerce</Heading>
+            <Text>Food truck delivery</Text>
+          </VStack>
+          <Divider marginY={6} />
+          <Component {...pageProps} />
+        </Container>
+      </Box>
+    </ChakraProvider>
+  )
 }
+
+export default App
