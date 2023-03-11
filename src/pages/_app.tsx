ea05ca84ec85aaa0d88cabf85b2cf67d11c802f6
5pamm3r <cmassarino.dev@gmail.com>
2023-03-11 17:03:34 -0300

    Move to component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,23 +1,26 @@
-import { Box, ChakraProvider, Container, Divider, Heading, Image, Text, VStack } from '@chakra-ui/react'
-import { AppProps } from 'next/app'
-import theme from '../../theme'
+import {
+  Box,
+  ChakraProvider,
+  Container,
+} from "@chakra-ui/react";
+import { AppProps } from "next/app";
+import theme from "../../theme";
 
 const App: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
     <ChakraProvider theme={theme}>
-      <Box padding={4}>
-        <Container backgroundColor='white' boxShadow='md' marginY={4} maxWidth='container.xl' padding={4}>
-          <VStack>
-            <Image borderRadius={9999} src='//placehold.it/128x128' />
-            <Heading>Ecommerce</Heading>
-            <Text>Food truck delivery</Text>
-          </VStack>
-          <Divider marginY={6} />
+      <Box>
+        <Container
+          backgroundColor="gray.100"
+          boxShadow="md"
+          maxWidth="container.xl"
+          padding={4}
+        >
           <Component {...pageProps} />
         </Container>
       </Box>
     </ChakraProvider>
-  )
-}
+  );
+};
 
-export default App
+export default App;
