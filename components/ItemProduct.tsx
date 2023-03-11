b03376be7310df3b5ca073568ed143ff9fb04a79
5pamm3r <cmassarino.dev@gmail.com>
2023-03-11 17:01:37 -0300

    Add ItemProduct comp

---
+++ components/ItemProduct.tsx
@@ -0,0 +1,54 @@
+import { HStack, Box, Image, VStack, Text, Button, } from "@chakra-ui/react"
+import { motion } from "framer-motion"
+import { Product } from "../products/types"
+import ProductModal from "./ProductModal"
+
+type ItemsProps = {
+  product: Product
+  handleAddToCart: any
+  setSelectedImage: any
+  parseCurrency: any
+}
+
+function ItemProduct({ product, handleAddToCart, setSelectedImage, parseCurrency }: ItemsProps) {
+  return (
+    <HStack
+      spacing={3}
+      padding={4}
+      borderRadius="md"
+      justify="space-between"
+    >
+      <Box w='120px' h='120px'>
+        <Image
+          w='100%'
+          as={motion.img}
+          cursor="pointer"
+          layoutId={product.image}
+          alt={product.title}
+          borderTopRadius="md"
+          maxHeight={128}
+          objectFit="cover"
+          src={product.image}
+          loading='lazy'
+          onClick={() => setSelectedImage(product.image)}
+        />
+      </Box>
+      <VStack spacing={1} align='start'>
+        <Text fontWeight='bold'>{product.title}</Text>
+        <Text fontSize="sm" fontWeight={500} color="green.500">
+          {parseCurrency(product.price)}
+        </Text>
+        <ProductModal title={product.title} content={product.description} />
+      </VStack>
+      <Button
+        colorScheme="primary"
+        size="sm"
+        variant="outline"
+        onClick={() => handleAddToCart(product)}
+      >
+        Add to cart
+      </Button>
+    </HStack>
+  )
+}
+export default ItemProduct
