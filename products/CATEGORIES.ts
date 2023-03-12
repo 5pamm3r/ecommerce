061b617cdc5bb4f1f6cd45a2c4e894b1e8deeeca
5pamm3r <cmassarino.dev@gmail.com>
2023-03-12 12:57:41 -0300

    Add categories

---
+++ products/CATEGORIES.ts
@@ -0,0 +1,24 @@
+import { Category } from "./typesCategory";
+
+export const CATEGORIES:Category[] = [
+  {
+    title: 'All',
+    image: 'https://img.icons8.com/color/48/null/refreshments.png'
+  },
+  {
+    title: 'Burger',
+    image: 'https://img.icons8.com/doodle/48/null/hamburger.png'
+  },
+  {
+    title: 'Pizza',
+    image: 'https://img.icons8.com/emoji/48/null/pizza-emoji.png'
+  },
+  {
+    title: 'Cocktail',
+    image: 'https://img.icons8.com/emoji/48/null/tropical-drink.png'
+  },
+  {
+    title: 'Dessert',
+    image: 'https://img.icons8.com/color/48/null/cake.png'
+  }
+]
