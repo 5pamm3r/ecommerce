b555031296ddb83e24e72f73a873144cc09eb229
5pamm3r <cmassarino.dev@gmail.com>
2023-03-12 12:56:05 -0300

    Destructuring

--- components/NavCategories.tsx
+++ components/NavCategories.tsx
@@ -1,29 +1,16 @@
-import { HStack, Text, Image, VStack, } from '@chakra-ui/react';
+import { HStack} from '@chakra-ui/react';
 import React from 'react';
+import { Category } from '../products/typesCategory';
 
-function NavCategories() {
+type NavCategoryProps = {
+  CATEGORIES: Category[];
+  render: any;
+}
+
+function NavCategories({ CATEGORIES, render }:NavCategoryProps ){
   return (
     <HStack mt={6} overflowY={'scroll'} whiteSpace={'nowrap'} spacing={4} justify='space-between'>
-      <VStack p={4} borderRadius='20px' _hover={{ backgroundColor: 'white' }}>
-        <Image minW='48px' src='https://img.icons8.com/color/48/null/refreshments.png' alt="All menu" />
-        <Text fontWeight='bold'>All</Text>
-      </VStack>
-      <VStack p={4} borderRadius='20px' _hover={{ backgroundColor: 'white' }}>
-        <Image minW='48px' src="https://img.icons8.com/doodle/48/null/hamburger.png" alt="Burger" />
-        <Text fontWeight='bold'>Burger</Text>
-      </VStack>
-      <VStack p={4} borderRadius='20px' _hover={{ backgroundColor: 'white' }}>
-        <Image minW='48px' src="https://img.icons8.com/emoji/48/null/pizza-emoji.png" alt="Pizza" />
-        <Text fontWeight='bold'>Pizza</Text>
-      </VStack>
-      <VStack p={4} borderRadius='20px' _hover={{ backgroundColor: 'white' }}>
-        <Image minW='48px' src='https://img.icons8.com/emoji/48/null/tropical-drink.png' alt="Cocktail" />
-        <Text fontWeight='bold'>Cocktail</Text>
-      </VStack>
-      <VStack p={4} borderRadius='20px' _hover={{ backgroundColor: 'white' }}>
-        <Image minW='48px' src='https://img.icons8.com/color/48/null/cake.png' alt="Desserts" />
-        <Text fontWeight='bold'>Desserts</Text>
-      </VStack>
+      { CATEGORIES.map(render) }
     </HStack>
   )
 }
