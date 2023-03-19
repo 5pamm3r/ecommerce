ca08e4c747c0d24b5f83d6157b698004adfdca16
5pamm3r <cmassarino.dev@gmail.com>
2023-03-18 23:10:39 -0300

    Add login page

---
+++ src/pages/login.tsx
@@ -0,0 +1,12 @@
+import { Heading } from '@chakra-ui/react'
+import React from 'react'
+
+interface Props {}
+
+const Login: React.FC<Props> = () => {
+  return (
+    <Heading as='h1'>Login</Heading>
+  )
+}
+
+export default Login
