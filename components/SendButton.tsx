74182e4ade709a76cd7f347be637445f948cd6fc
5pamm3r <cmassarino.dev@gmail.com>
2023-03-18 23:09:53 -0300

    Fix design

--- components/SendButton.tsx
+++ components/SendButton.tsx
@@ -1,5 +1,5 @@
 import { Button, Image, Link, Stack, } from "@chakra-ui/react"
-import { motion } from "framer-motion"
+import { AnimatePresence, motion } from "framer-motion"
 import React from "react"
 
 interface Props {
@@ -10,29 +10,31 @@ interface Props {
 
 const SendButton: React.FC<Props> = ({ text, totalPrice, totalItems }) => {
   return (
-    <Stack
-      align="center"
-      bottom={4}
-      justify="center"
-      position="sticky"
-      as={motion.div}
-      initial={{ scale: 0 }}
-      animate={{ scale: 1 }}
-      exit={{ scale: 0 }}
-    >
-      <Button
-        isExternal
-        as={Link}
-        colorScheme="whatsapp"
-        width="fit-content"
-        href={`https://wa.me/${process.env.NEXT_PUBLIC_NUMBER}?text=${encodeURIComponent(text)}`}
-        leftIcon={
-          <Image src="https://icongr.am/fontawesome/whatsapp.svg?size=32&color=ffffff" alt='' />
-        }
+    <AnimatePresence>
+      <Stack
+        align="center"
+        bottom={4}
+        justify="center"
+        position="sticky"
+        as={motion.div}
+        initial={{ scale: 0 }}
+        animate={{ scale: 1 }}
+        exit={{ scale: 0 }}
       >
-        {totalItems} items ({totalPrice})
-      </Button>
-    </Stack>
+        <Button
+          isExternal
+          as={Link}
+          colorScheme="whatsapp"
+          width="fit-content"
+          href={`https://wa.me/${process.env.NEXT_PUBLIC_NUMBER}?text=${encodeURIComponent(text)}`}
+          leftIcon={
+            <Image src="https://icongr.am/fontawesome/whatsapp.svg?size=32&color=ffffff" alt='' boxSize='28px' />
+          }
+        >
+          {totalItems} items ({totalPrice})
+        </Button>
+      </Stack>
+    </AnimatePresence>
   )
 }
 
