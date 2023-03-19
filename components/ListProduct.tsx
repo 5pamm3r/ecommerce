be78277a0baa64e0e83093a612af2150b6289420
5pamm3r <cmassarino.dev@gmail.com>
2023-03-18 23:08:31 -0300

    Fix responsive with desktop

--- components/ListProduct.tsx
+++ components/ListProduct.tsx
@@ -1,22 +1,26 @@
-import { Grid } from "@chakra-ui/react"
+import { Grid, Stack } from "@chakra-ui/react"
 import React from "react";
 import { Product } from "../products/typesProduct"
 
 interface Props {
   productsSelected: Product[];
-  render: (value: Product, index: number, array:Product[]) => React.ReactNode;
+  render: (value: Product, index: number, array: Product[]) => React.ReactNode;
+  children: React.ReactNode;
 }
 
-const ListProduct: React.FC<Props> = ({ productsSelected, render }) => {
+const ListProduct: React.FC<Props> = ({ productsSelected, render, children }) => {
   return (
-    <Grid
-      gridGap={2}
-      templateColumns='1fr'
-      borderTopRadius="20px"
+    <Stack spacing={6} pt={6}>
+      <Grid
+        gridGap={2}
+        templateColumns={{ sm: '1fr', md: 'repeat(3,1fr)', lg: 'repeat(4,1fr)', xl: 'repeat(5,1fr)', '2xl': 'repeat(5,1fr)'}}
+        borderTopRadius="20px"
 
-    >
-      {productsSelected.map(render)}
-    </Grid>
+      >
+        {productsSelected.map(render)}
+      </Grid>
+      {children}
+    </Stack>
   )
 }
 
