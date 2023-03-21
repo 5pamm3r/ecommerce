7aa17375fc7029005ce9d52a276fb6b5b86f0122
5pamm3r <cmassarino.dev@gmail.com>
2023-03-20 22:38:32 -0300

    cursor

--- components/ItemCategory.tsx
+++ components/ItemCategory.tsx
@@ -1,22 +1,34 @@
-import { VStack, Image, Text } from "@chakra-ui/react"
+import { VStack, Image, Text } from "@chakra-ui/react";
 import React from "react";
 import { Product } from "../products/typesProduct";
 
 interface Props {
-  title: Product['title'];
-  image: Product['image'];
+  title: Product["title"];
+  image: Product["image"];
   changeProduct: VoidFunction;
 }
 
 const ItemCategory: React.FC<Props> = ({ title, image, changeProduct }) => {
   return (
     <>
-      <VStack fontSize={['sm', 'md']} p={4} borderRadius='20px' _hover={{ backgroundColor: 'white' }} onClick={(e)=>changeProduct()}>
-          <Image w={['30px', '40px', '50px']} minW='30px' src={image} alt="All menu" />
-          <Text fontWeight='bold'>{title}</Text>
+      <VStack
+        fontSize={["sm", "md"]}
+        p={4}
+        borderRadius="20px"
+        _hover={{ backgroundColor: "white" }}
+        onClick={(e) => changeProduct()}
+        cursor='pointer'
+      >
+        <Image
+          w={["30px", "40px", "50px"]}
+          minW="30px"
+          src={image}
+          alt="All menu"
+        />
+        <Text fontWeight="bold">{title}</Text>
       </VStack>
     </>
-  )
-}
+  );
+};
 
-export default ItemCategory
+export default ItemCategory;
