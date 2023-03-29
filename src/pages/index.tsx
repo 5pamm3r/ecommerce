42203f3147ec6f81e42f8f11a30f5ef41be41854
5pamm3r <cmassarino.dev@gmail.com>
2023-03-29 00:11:41 -0300

    Add editAddress

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,37 +1,98 @@
-import { FormControl, FormLabel, Heading, Input, Button, Grid, Stack } from '@chakra-ui/react'
-import React from 'react'
-import { useRouter } from 'next/router'
-import Head from 'next/head'
+import {
+  FormControl,
+  FormLabel,
+  Heading,
+  Input,
+  Button,
+  Grid,
+  Stack,
+  InputLeftElement,
+  InputGroup,
+  Text,
+} from "@chakra-ui/react";
+import React from "react";
+import { useRouter } from "next/router";
+import Head from "next/head";
+import { BiUser, BiMapPin } from "react-icons/bi";
 
 interface Props { }
 
 const Login: React.FC<Props> = () => {
-  const [address, setAddress] = React.useState<string>('')
-  const router = useRouter()
+  const [address, setAddress] = React.useState<string>("");
+  const [username, setUsername] = React.useState<string>("");
+  const [error, setError] = React.useState<boolean>(false);
+  const router = useRouter();
 
   const onSubmit = (e: React.FormEvent<HTMLButtonElement>) => {
     e.preventDefault();
-    router.push(`/main?address=${encodeURIComponent(address)}`)
-  }
+    if (!address || !username) {
+      setError(true);
+      return;
+    }
+    setError(false);
+    router.push(`/main?username=${encodeURIComponent(username)}&address=${encodeURIComponent(address)}`)
+  };
   return (
-    <Stack maxW='800px' m='0 auto' backgroundColor='gray.100'>
+    <Stack maxW="800px" m="0 auto" backgroundColor="gray.100">
       <Head>
         <title>Log in</title>
       </Head>
-      <Grid placeContent='center' justifyItems='center' h='100vh'>
-        <Heading as='h1'>Log in</Heading>
-        <FormControl isRequired mt={8} >
-          <FormLabel htmlFor='username'>Username</FormLabel>
-          <Input id='username' placeholder='' isRequired autoFocus />
-          <FormLabel htmlFor='address'>Address</FormLabel>
-          <Input id='address' placeholder='' value={address} onChange={e=>setAddress(e.target.value)}/>
-          <Button type='submit' colorScheme='teal' m='32px auto 0' display='block' onClick={onSubmit} >
+      <Grid placeContent="center" h="100vh">
+        <Heading as="h1">Sign in</Heading>
+        <FormControl isRequired mt={8}>
+          <FormLabel htmlFor="username">Username</FormLabel>
+          <InputGroup>
+            <InputLeftElement
+              pointerEvents="none"
+              children={<BiUser color="gray" />}
+            />
+            <Input
+              type="text"
+              id="username"
+              placeholder=""
+              required
+              autoFocus
+              backgroundColor="white"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </InputGroup>
+          <FormLabel htmlFor="address" mt={2}>
+            Address
+          </FormLabel>
+          <InputGroup>
+            <InputLeftElement
+              pointerEvents="none"
+              children={<BiMapPin color="gray" />}
+            />
+            <Input
+              type="text"
+              id="address"
+              placeholder=""
+              value={address}
+              backgroundColor="white"
+              required
+              onChange={(e) => setAddress(e.target.value)}
+            />
+          </InputGroup>
+          {!!error && (
+            <Text color="red" mt={2}>
+              All fields are required.
+            </Text>
+          )}
+          <Button
+            type="submit"
+            colorScheme="orange"
+            w="100%"
+            mt={8}
+            onClick={onSubmit}
+          >
             Send
           </Button>
         </FormControl>
       </Grid>
     </Stack>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
