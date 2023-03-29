c8c403cfcf2aa39225fcadda1fc91ca6a224c174
5pamm3r <cmassarino.dev@gmail.com>
2023-03-29 00:08:56 -0300

    Add popover

---
+++ components/Header/PopoverAddress.tsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import { Popover, Box, PopoverTrigger, IconButton, PopoverContent, FocusLock, PopoverArrow, PopoverCloseButton, useDisclosure } from '@chakra-ui/react'
+import { ChevronDownIcon } from '@chakra-ui/icons';
+import FormEditAddress from './FormEditAddress';
+
+
+interface Props {
+  isOpen: boolean
+  onOpen: VoidFunction
+  onClose: VoidFunction
+  firstFieldRef: any
+  address: string
+  children: React.ReactNode
+}
+
+const PopoverAddress: React.FC<Props> = ({ isOpen, onOpen, onClose, firstFieldRef, children }) => {
+
+  return (
+    <Box>
+      <Popover
+        isOpen={isOpen}
+        initialFocusRef={firstFieldRef}
+        onOpen={onOpen}
+        onClose={onClose}
+        placement="top"
+        closeOnBlur={false}
+      >
+        <PopoverTrigger>
+          <IconButton
+            color='whiteAlpha.800'
+            backgroundColor='transparent'
+            size="sm"
+            aria-label="Edit address"
+            _hover={{ backgroundColor: 'gray' }}
+            icon={<ChevronDownIcon />}
+          />
+        </PopoverTrigger>
+        <PopoverContent p={5}>
+          <FocusLock persistentFocus={false}>
+            <PopoverArrow />
+            <PopoverCloseButton />
+            {children}
+          </FocusLock>
+        </PopoverContent>
+      </Popover>
+    </Box>
+  )
+}
+
+export default PopoverAddress
