c062703a81d09a9305a71e22b00850f8df6715c8
5pamm3r <cmassarino.dev@gmail.com>
2023-03-29 00:05:03 -0300

    Limit items

--- components/ListProduct.tsx
+++ components/ListProduct.tsx
@@ -6,18 +6,19 @@ interface Props {
   productsSelected: Product[];
   render: (value: Product, index: number, array: Product[]) => React.ReactNode;
   children: React.ReactNode;
+  numItems: number;
 }
 
-const ListProduct: React.FC<Props> = ({ productsSelected, render, children }) => {
+const ListProduct: React.FC<Props> = ({ productsSelected, render, children, numItems }) => {
   return (
-    <Stack spacing={6} pt={6}>
+    <Stack spacing={6} pt={1}>
       <Grid
         gridGap={2}
-        templateColumns={{ sm: '1fr', md: 'repeat(3,1fr)', lg: 'repeat(4,1fr)', xl: 'repeat(5,1fr)', '2xl': 'repeat(5,1fr)'}}
+        templateColumns={{ sm: '1fr', md: 'repeat(3,1fr)', lg: 'repeat(4,1fr)', xl: 'repeat(5,1fr)', '2xl': 'repeat(5,1fr)' }}
         borderTopRadius="20px"
 
       >
-        {productsSelected.map(render)}
+        {productsSelected.slice(0, numItems).map(render)}
       </Grid>
       {children}
     </Stack>
